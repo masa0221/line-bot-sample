a5e29b34cfc2556d2c3cb3fbdb0bdc1716b79888
Masashi Tsuru <1150769+masa0221@users.noreply.github.com>
2021-02-13 21:11:57 +0900

    ボットを友だち登録している全員にメッセージを配信する処理を実装

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class LineController extends Controller
 {
     // メッセージ送信
     public function delivery()
     {
-        // TODO: ここに具体的に実装
-
         // 1. 登録されている友だちにメッセージを送信
+        $httpClient = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
+        $textBuilder = new TextMessageBuilder('Hello LINE!');
+        $bot->broadcast($textBuilder);
 
         return response()->json(['message' => 'sent']);
     }
