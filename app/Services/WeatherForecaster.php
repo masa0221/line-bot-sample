e75a1e72848f6a4d73ecc193e726713b4c6292fa
Masashi Tsuru <1150769+masa0221@users.noreply.github.com>
2021-02-14 15:22:17 +0900

    天気予報を提供するサービスクラスを作成

---
+++ app/Services/WeatherForecaster.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\ExternalApis\WeatherForecastApi;
+
+class WeatherForecaster {
+    private $weatherForecastApi;
+    private $latitude;
+    private $longitude;
+
+    /**
+     * @param WeatherForecastApi 天気予報API
+     * @param float $latitude 緯度
+     * @param float $longitude 軽度
+     */
+    public function __construct(WeatherForecastApi $weatherForecastApi, $latitude, $longitude)
+    {
+        $this->weatherForecastApi = $weatherForecastApi;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * 指定された緯度・経度から天気予報を取得
+     *
+     * @return string 天気予報の文字
+     */
+    public function forecast()
+    {
+        $weacherJson = $this->weatherForecastApi->fetch(
+            $this->latitude,
+            $this->longitude
+        );
+
+        return $this->weatherForecastApi->parse($weacherJson);
+    }
+}
