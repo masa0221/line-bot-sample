9aae8dc4a6fab65188a8923171426b98fdd8ebc5
Masashi Tsuru <1150769+masa0221@users.noreply.github.com>
2021-02-14 10:54:10 +0900

    不要なデバッグログを削除

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -7,7 +7,6 @@ use App\Services\Deliver;
 use App\Services\ReplyMessageGenerator;
 use App\Services\RequestParser;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class LineController extends Controller
 {
@@ -25,7 +24,6 @@ class LineController extends Controller
     public function callback(Request $request)
     {
         // 1. 受け取った情報からメッセージの情報を取り出す
-        Log::debug('RequestBody: ' . $request->getContent());
         $parser = new RequestParser($request->getContent());
         $recievedMessages = $parser->getRecievedMessages();
 
@@ -38,10 +36,6 @@ class LineController extends Controller
         foreach ($recievedMessages as $recievedMessage) {
             // 2. 受け取ったメッセージの内容から返信するメッセージを生成
             $replyMessage = $replyMessageGenerator->generate($recievedMessage->getText());
-
-            Log::debug('message.text: ' .$recievedMessage->getText());
-            Log::debug('replyMessage: ' . $replyMessage);
-            Log::debug('replyToken: ' . $recievedMessage->getReplyToken());
 
             // 3. 返信メッセージを返信先に送信
             $deliver->reply($recievedMessage->getReplyToken(), $replyMessage);
