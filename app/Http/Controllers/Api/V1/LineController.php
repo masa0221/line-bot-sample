6f9364575d089b2bebade0d42911fe0756368cb0
Masashi Tsuru <1150769+masa0221@users.noreply.github.com>
2021-02-14 09:24:23 +0900

    Webhook実装2: 受信したメッセージに対して返信する処理を実装

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
@@ -25,11 +26,59 @@ class LineController extends Controller
     // メッセージを受け取って返信
     public function callback(Request $request)
     {
-        // TODO: ここに具体的に実装
-
         // 1. 受け取った情報からメッセージの情報を取り出す
-        // 2. 受け取ったメッセージの内容から返信するメッセージを生成
-        // 3. 返信メッセージを返信先に送信
+        Log::debug('RequestBody: ' . $request->getContent());
+        $eventsObj = json_decode($request->getContent());
+        $replyToken = '';
+        $replyMessage = '';
+        if (is_null($eventsObj) || is_null($eventsObj->events)) {
+            return response()->json(['message' => 'received(no events)']);
+        }
+
+        foreach ($eventsObj->events as $event) {
+            if ($event->type == 'message') {
+                $replyToken = $event->replyToken;
+                $message = $event->message;
+
+                // 2. 受け取ったメッセージの内容から返信するメッセージを生成
+                switch ($message->text) {
+                    case '今日の天気は？':
+                        // 天気APIを使って情報を取得してきたら正しい情報にできる
+                        $replyMessage = 'は、晴れかな・・・（しらんけど）';
+                        break;
+                    case '元気？':
+                        $replyMessage = 'はい、元気です。あなたは？';
+                        break;
+                    case '後ウマイヤ朝の最盛期王は？':
+                        $replyMessage = 'アブド＝アッラフマーン３世';
+                        break;
+                    default:
+                        if (strpos($message->text, '？') !== false) {
+                            // 疑問符が含まれている場合(部分一致)
+                            $replyMessage = '「今日の天気は？」という質問に答える事ができますよ！';
+                        } else {
+                            $replyMessage = 'すみません、よくわかりません';
+                        }
+                }
+            }
+
+            Log::debug('message.text: ' . $message->text);
+            Log::debug('replyMessage: ' . $replyMessage);
+            Log::debug('replyToken: ' . $replyToken);
+
+            // 3. 返信メッセージを返信先に送信
+            if ($replyToken && $replyMessage) {
+                $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
+                $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
+
+                $textMessageBuilder = new TextMessageBuilder($replyMessage);
+                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+                if (!$response->isSucceeded()) {
+                    Log::error($response->getRawBody());
+                }
+            }
+        }
 
         return response()->json(['message' => 'received']);
     }
